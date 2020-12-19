ad220a8ce61a073bf5691f54fdaca07c3fac4e03
Marcos1202 <juanmarcos.castro97@gmail.com>
2020-12-18 18:55:27 -0600

    first commit

---
+++ js/menu.js
@@ -0,0 +1,51 @@
+// Variables
+
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlaces');
+let abrir = document.getElementById('open');
+let botones = document.getElementsByClassName('btn-header');
+let cerrado = true;
+
+
+
+function apertura(){
+    if(cerrado){
+        menu.style.width = '70vw';
+        cerrado = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = 'hidden';
+        cerrado = true;
+    }
+}
+
+window.addEventListener('load', function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+    
+});
+window.addEventListener('click',function(e){
+   
+    if(cerrado==false){
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            menu.style.width = '0%';
+            menu.style.overflow = 'hidden';
+            cerrado = true;
+        }
+    }
+});
+/* window.addEventListener('scroll', function(){
+   
+
+}); */
+window.addEventListener('resize', function(){
+    if(screen.width>= 700){
+        cerrado = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('width');
+    }
+});
+abrir.addEventListener('click', function(){
+    apertura();
+});
